7afd8e249c9cd5e0a3785742891b255ad9715938
Evgeniy Salish <jeka5650962@gmail.com>
2023-01-29 20:13:05 +0300

    290123 2013

--- js/header.js
+++ js/header.js
@@ -1,7 +1,6 @@
 let scrollPosition = window.scrollY
 
 const header = document.querySelector(".header")
-const scrollChange = 1
 
 const add_class_on_scroll = () => header.classList.add("bg-green")
 const remove_class_on_scroll = () => header.classList.remove("bg-green")
@@ -9,7 +8,7 @@ const remove_class_on_scroll = () => header.classList.remove("bg-green")
 window.addEventListener('scroll', function () {
     scrollPosition = window.scrollY
 
-    if (scrollPosition >= scrollChange) {
+    if (scrollPosition >= 490) {
         add_class_on_scroll()
     } else {
         remove_class_on_scroll()
