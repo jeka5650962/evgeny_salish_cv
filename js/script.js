136523388ea9dbdc598e475f14fd18608b881279
Evgeniy Salish <jeka5650962@gmail.com>
2023-01-30 15:46:11 +0300

    300123 1546

--- js/script.js
+++ js/script.js
@@ -1,4 +1,22 @@
-'use strict'
+/* ------------------------------- burger ------------------------------- */
+
+let headerBurger = document.querySelector('.header__burger')
+let headerMenu = document.querySelector('.header__menu')
+let back = document.querySelector('body')
+let headerList = document.querySelector('.header__list')
+
+headerBurger.onclick = function () {
+    headerBurger.classList.toggle('active')
+    headerMenu.classList.toggle('active')
+    back.classList.toggle('lock')
+}
+
+headerList.onclick = function () {
+    headerList.classList.remove('active')
+    back.classList.toggle('lock')
+}
+
+/* ------------------------------- form ------------------------------- */
 
 document.addEventListener('DOMContentLoaded', function () {
     const form = document.getElementById('form')
@@ -65,3 +83,38 @@ document.addEventListener('DOMContentLoaded', function () {
         return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value)
     }
 })
+
+/* ------------------------------- header ------------------------------- */
+
+let scrollPosition = window.scrollY
+
+const header = document.querySelector(".header")
+
+const add_class_on_scroll = () => header.classList.add("bg-green")
+const remove_class_on_scroll = () => header.classList.remove("bg-green")
+
+window.addEventListener('scroll', function () {
+    scrollPosition = window.scrollY
+
+    if (scrollPosition >= 490) {
+        add_class_on_scroll()
+    } else {
+        remove_class_on_scroll()
+    }
+
+})
+
+/* ------------------------------- scroll ------------------------------- */
+
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault()
+        const blockId = anchor.getAttribute('href')
+        document.querySelector('' + blockId).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        })
+    })
+}
