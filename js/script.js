396115b3247714752740fa852a9a04a27b4cc49f
Evgeniy Salish <jeka5650962@gmail.com>
2023-02-02 13:57:03 +0300

    020223 1356

--- js/script.js
+++ js/script.js
@@ -2,18 +2,18 @@
 
 let headerBurger = document.querySelector('.header__burger')
 let headerMenu = document.querySelector('.header__menu')
-let back = document.querySelector('body')
+// let back = document.querySelector('body')
 let headerList = document.querySelector('.header__list')
 
 headerBurger.onclick = function () {
     headerBurger.classList.toggle('active')
     headerMenu.classList.toggle('active')
-    back.classList.toggle('lock')
+    // back.classList.toggle('lock')
 }
 
 headerList.onclick = function () {
     headerList.classList.remove('active')
-    back.classList.toggle('lock')
+    // back.classList.toggle('lock')
 }
 
 /* ------------------------------- form ------------------------------- */
